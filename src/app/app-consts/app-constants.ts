181719a0dab1568a9b4c45ab3d884572f4634131
irzhakbasil <irzhakbasil@gmail.com>
2022-08-01 04:47:57 +0300

    cleanup

--- src/app/app-consts/app-constants.ts
+++ src/app/app-consts/app-constants.ts
@@ -4,7 +4,7 @@ export const CREATE_QUESTION_MIN_FILLED_IN_INPUTS = 2;
 export enum AppRoutesEnum {
     MANAGE_QUESTIONS_ROOT = '/manage-questions',
     MANAGE_QUESTIONS_RELATIVE = 'manage-questions',
-    CREATE_QUESTION = 'manage-questions/create-question',
+    CREATE_QUESTION_RELATIVE = 'create-question',
     EDIT_QUESTION = 'manage-questions/edit-question/:id',
     LIST_OF_QUESTIONS_RElATIVE = 'list-of-questions',
     LIST_OF_QUESTIONS_ROOT = '/list-of-questions',
